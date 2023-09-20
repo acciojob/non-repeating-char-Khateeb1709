4550282d51e5b07e8f03bce7fe08d7be06600a88
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-09-20 13:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 for(let i=0;i<str.length-1;i++){
+		 let curr = str.charAt(i);
+         let next = str.charAt(i+1);
+		 if(curr != next){
+			 return curr;
+		 }
+		 else{
+			 while(str.charAt(i)==curr)i++;
+		 }
+	 }
+	 return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
