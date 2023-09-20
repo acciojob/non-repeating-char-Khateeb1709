d412198ff245e3a9e2ee7770d5b8ba8cb4918ee4
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-09-20 13:59:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 if(str.length == 0)return "null";
+	 if(str.length == 1)return str.charAt(0);
+	
 	 for(let i=0;i<str.length-1;){
 		 let curr = str.charAt(i);
          let next = str.charAt(i+1);
